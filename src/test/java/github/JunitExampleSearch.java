cdcf93a454ab6d5efa02f8ca665948d306b5f348
MargaritaMalakhova <ritulek@mail.ru>
2023-04-12 10:53:46 +0200

    fixed test JunitExampleSearch

--- src/test/java/github/JunitExampleSearch.java
+++ src/test/java/github/JunitExampleSearch.java
@@ -1,5 +1,7 @@
 package github;
 
+import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import static com.codeborne.selenide.Condition.*;
@@ -7,6 +9,11 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.*;
 
 public class JunitExampleSearch {
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.holdBrowserOpen = true;
+        Configuration.browserSize = "1920x1080";
+    }
     @Test
     void junitExampleShouldBeFound() {
         //open Selenide project page
@@ -14,10 +21,9 @@ public class JunitExampleSearch {
         //click on Wiki section
         $("#wiki-tab").click();
         //insure SoftAssertions page is in the Pages list
-        $("#wiki-pages-box").$(byText("Show 2 more pages…")).click();
+        $(".wiki-rightbar").$("button").shouldHave(text(" Show 2 more pages")).click();
         //open SoftAssertions, check code example for JUnit5
-        $("#wiki-pages-box").$(byText("SoftAssertions")).click();
+        $("div#wiki-pages-box").$(byText("SoftAssertions")).click();
         $(".markdown-body").shouldHave(text("Using JUnit5 extend test class"));
-        sleep(5000);
     }
 }
